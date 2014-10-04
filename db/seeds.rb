3a4078ec21784cc05b6e80776084beae3251e2b9
Sushant <sushant.dinesh94@gmail.com>
2014-10-04 17:46:54 +0530

    Added changes to push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+u = User.new
+u.email = "foo@bar.com"
+u.password = 'connect123'
+u.save
