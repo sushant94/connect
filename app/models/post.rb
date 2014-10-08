c053bdc24ba9d2d94c23a63e0f2dc3c0985b93c6
Sushant <sushant.dinesh94@gmail.com>
2014-10-09 02:43:11 +0530

    Fixed Issue#2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,10 @@
 class Post < ActiveRecord::Base
-    validates :title,:author,:content,:event, presence: true
+    validates :title,:author,:content, presence: true
     validate :validate_event
 
     def validate_event
         if event
-            if event_start > event_end
-                errors.add(:event_start, "cannot be after end time.")
-            elsif event_start.to_i < Time.now.to_i
+            if event_start.to_i < Time.now.to_i
                 errors.add(:event_start, "cannot be before present.")
             end
         end
