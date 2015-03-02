9efa93e8973f07a6b94db074b021dcfd1f2d716d
chinmaydd <chinmay1dd@gmail.com>
2015-03-02 23:06:45 +0530

    Testing live_news

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+    after_create :notify_users
+
     def validate_event
         if event
             if event_start.to_i < Time.now.to_i
