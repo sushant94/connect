967246a7922ec9422e6ff7f953e24b74fb6d382b
Sushant <sushant.dinesh94@gmail.com>
2015-03-02 17:42:48 +0530

    Revert "Fixed noobout"
    This reverts commit 38e0cf97ed0d113247552f9977621240dab7d93d.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,6 @@ class Post < ActiveRecord::Base
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-    after_create :notify_users
-
     def validate_event
         if event
             if event_start.to_i < Time.now.to_i
@@ -14,13 +12,6 @@ class Post < ActiveRecord::Base
             end
         end
         return true
-    end
-
-
-    def notify_users
-        filtered = self.content
-        filtered.gsub!(/[^a-zA-Z0-9]/, "")
-        `node send.js #{self.title} #{truncate(filtered, 30)}`
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,21 +29,6 @@ ActiveRecord::Schema.define(version: 20150301173335) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
-  create_table "feeds", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "lives", force: true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.integer  "feed_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
